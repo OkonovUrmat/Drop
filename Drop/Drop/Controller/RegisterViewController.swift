cefa33d93d672076b4e4390c44a8f0592cfd64fa
OkonovUrmat <okonov.okonov@gmail.com>
2021-02-07 17:03:51 +0600

    Create RegisterViewController

---
+++ Drop/Drop/Controller/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  Drop
+//
+//  Created by Urmat on 2/7/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
