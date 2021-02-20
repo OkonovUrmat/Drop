0eaecc545c9e97a31d00feda50cb3c860a17d172
OkonovUrmat <okonov.okonov@gmail.com>
2021-02-20 22:53:04 +0600

    Change design of RegistrationView

--- Drop/Drop/Controller/RegisterViewController.swift
+++ Drop/Drop/Controller/RegisterViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var nameLbl: RoundedTextField!
+    @IBOutlet weak var phoneNumberLbl: RoundedTextField!
+    @IBOutlet weak var passwordLbl: RoundedTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func onClickRegistrationBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onClickRuleBtn(_ sender: Any) {
+        
     }
 }
