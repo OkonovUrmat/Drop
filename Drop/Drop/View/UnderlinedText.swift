c15688b7239e8229dcca701fc4fb3d53a56beccd
OkonovUrmat <okonov.okonov@gmail.com>
2021-05-03 23:49:51 +0600

    Add UnderLinedText

---
+++ Drop/Drop/View/UnderlinedText.swift
@@ -0,0 +1,24 @@
+//
+//  UnderlinedText.swift
+//  Drop
+//
+//  Created by Urmat on 4/29/21.
+//
+
+import UIKit
+
+class UnderlinedLabel: UIButton {
+
+override var text: String? {
+    didSet {
+        guard let text = text else { return }
+        let textRange = NSRange(location: 0, length: text.count)
+        let attributedText = NSMutableAttributedString(string: text)
+        attributedText.addAttribute(.underlineStyle,
+                                    value: NSUnderlineStyle.single.rawValue,
+                                    range: textRange)
+        // Add other attributes if needed
+        self.attributedText = attributedText
+        }
+    }
+}
