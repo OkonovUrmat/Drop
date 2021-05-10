b78d690f77c39d156873c87866d73eb9c9de2edb
OkonovUrmat <okonov.okonov@gmail.com>
2021-05-10 17:55:06 +0600

    UserRegisterViewController: Set underlined text for logInBtn

--- Drop/Drop/Controller/UserRegisterViewController.swift
+++ Drop/Drop/Controller/UserRegisterViewController.swift
@@ -12,9 +12,29 @@ class UserRegisterViewController: BaseViewController {
     @IBOutlet weak var nameLbl: RoundedTextField!
     @IBOutlet weak var phoneNumberLbl: RoundedTextField!
     @IBOutlet weak var passwordLbl: RoundedTextField!
+    @IBOutlet weak var logInBtn: UIButton!
     
+    // START: Variables for underline text
+    var attrs = [
+        NSAttributedString.Key.font : UIFont.systemFont(ofSize: 19.0),
+        NSAttributedString.Key.foregroundColor : UIColor.red,
+        NSAttributedString.Key.underlineStyle : 1] as [NSAttributedString.Key : Any]
+    
+    var logInAttributedString = NSMutableAttributedString(string:"")
+    
+    // END: Variables for underline text
+
     override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
+
+        // Underline text for buttons
+        self.underlineText()
+    }
+    
+    func underlineText() {
+        let logInButtonTitleStr = NSMutableAttributedString(string:"Уже есть аккаунт? Войти", attributes:attrs)
+        logInAttributedString.append(logInButtonTitleStr)
+        logInBtn.setAttributedTitle(logInButtonTitleStr, for: .normal)
     }
     
     @IBAction func onClickRegistrationBtn(_ sender: Any) {
