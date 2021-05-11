3c93bc584c92378122b91ec47199113cbced9cad
OkonovUrmat <okonov.okonov@gmail.com>
2021-05-11 21:18:28 +0600

    Add some Constants

--- Drop/Drop/Utilities/Constants.swift
+++ Drop/Drop/Utilities/Constants.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import Alamofire
+
+// Check if handler succeed
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
