3eafe6b7ac097bf61ccbdfe83a1575e69d9eb29d
OkonovUrmat <okonov.okonov@gmail.com>
2021-05-11 17:19:20 +0600

    Create OrderViewController

---
+++ Drop/Drop/Controller/OrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderViewController.swift
+//  Drop
+//
+//  Created by Urmat on 5/11/21.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
