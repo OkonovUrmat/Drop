ab4f5287fc7839fce8b87636655e60c3eb17dfaf
OkonovUrmat <okonov.okonov@gmail.com>
2021-05-15 17:54:38 +0600

    OrderDetailViewController: Outlets and variables

--- Drop/Drop/Controller/OrderDetailViewController.swift
+++ Drop/Drop/Controller/OrderDetailViewController.swift
@@ -8,8 +8,24 @@
 import UIKit
 
 class OrderDetailViewController: BaseViewController {
-
+    @IBOutlet weak var orderImg: UIImageView!
+    @IBOutlet weak var orderNameLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var orderDetailTableView: UITableView!
+    
+    var name: String = ""
+    var price: String = ""
+    var initialPoint: String = ""
+    var destinationPoint: String = ""
+    var time: String = ""
+    var date: String = ""
+    var car: String = ""
+    var details: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func connectWithOrderClicked(_ sender: Any) {
     }
 }
