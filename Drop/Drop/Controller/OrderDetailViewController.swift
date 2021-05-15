a2fe3c354638bbe018bc928d043e45e6306398c7
OkonovUrmat <okonov.okonov@gmail.com>
2021-05-15 17:49:41 +0600

    OrderDetailViewController: Remove unused functions

--- Drop/Drop/Controller/OrderDetailViewController.swift
+++ Drop/Drop/Controller/OrderDetailViewController.swift
@@ -9,16 +9,7 @@ import UIKit
 
 class OrderDetailViewController: BaseViewController {
 
-    static func create () -> OrderDetailViewController {
-        let orderDetailViewController: OrderDetailViewController = OrderDetailViewController(nibName: "OrderDetailViewController", bundle: nil);
-
-        return orderDetailViewController;
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
 }
